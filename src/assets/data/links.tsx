41df5469509e3e34953a9a780985b0b209ec33d4
estebanthi <esteban.thilliez@gmail.com>
2023-07-22 13:40:44 +0200

    BMAC

--- src/assets/data/links.tsx
+++ src/assets/data/links.tsx
@@ -1,4 +1,5 @@
 import {Github, Linkedin, Mailbox, Medium, Youtube} from "react-bootstrap-icons";
+import BMAC from "../../components/Icons/BMAC";
 
 const links = [
     {
@@ -17,6 +18,10 @@ const links = [
         icon: <Youtube/>,
         link: "https://www.youtube.com/channel/UCuDAZiZ79BkBFPrsG4N5QoQ"
     },
+    {
+        icon: <BMAC/>,
+        link: "https://www.buymeacoffee.com/estebanthi"
+    }
 ];
 
 
