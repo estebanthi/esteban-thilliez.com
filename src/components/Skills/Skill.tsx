4bdee633c521493f98757e80dc55507a9633f2b6
estebanthi <esteban.thilliez@gmail.com>
2023-07-29 23:00:55 +0200

    Mobile version

--- src/components/Skills/Skill.tsx
+++ src/components/Skills/Skill.tsx
@@ -19,9 +19,11 @@ const Skill = ({ icon, name, color }: SkillProps) => {
     const id = generateId();
 
     return (
+        <div>
         <div className={"Skill " + color} data-tooltip-id={id} data-tooltip-content={name} data-tooltip-place={"bottom"}>
             <img src={icon} alt={name} />
             <Tooltip id={id} className="tooltip"/>
+        </div>
         </div>
     );
 }
