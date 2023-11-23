dc474cc5a26efd642201a508465665fb06c3ec62
estebanthi <esteban.thilliez@gmail.com>
2023-11-23 11:20:35 +0100

    Fixed language change bug

--- src/components/Main/Contact.tsx
+++ src/components/Main/Contact.tsx
@@ -5,17 +5,21 @@ import "./Contact.css";
 import { useTranslation } from "react-i18next";
 import {TypeAnimation} from "react-type-animation";
 import ContactForm from "../Forms/ContactForm";
+import i18n from "i18next";
 
 
 const Contact: React.FC = () => {
 
         const { t } = useTranslation();
         const [typeAnimation, setTypeAnimation] = React.useState<React.ReactNode>(<></>);
+        i18n.on('languageChanged', () => {
+            setTypeAnimation(<span></span>);
+        });
 
         useEffect(() => {
         setTypeAnimation(<span></span>);
         setTimeout(() => {
-            setTypeAnimation(<TypeAnimation sequence={[t('contact.seq1'),t('contact.seq2'),t('contact.seq3'),t('contact.seq4'),]} wrapper="h3" repeat={Infinity} className="color-3" speed={35} deletionSpeed={1}/>);
+            setTypeAnimation(<TypeAnimation sequence={[t('contact.seq1'),t('contact.seq2'),t('contact.seq3'),t('contact.seq4'),]} wrapper="h3" repeat={Infinity} className="color-3" speed={1} deletionSpeed={1} cursor={true}/>);
             }, 0);
         }, [t]);
 
